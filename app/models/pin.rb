7d14aff552b8bbb0e33dfb81a2732096a07fae1c
Andrew Borstein <info@brownborgstudios.com>
2014-12-02 17:36:18 -0500

    Revert 263aaca..5c8e4fa
    This rolls back to commit 263aacad34af4e4620c36276125e62ff05f32bf7.

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,6 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, default_url: "http://placehold.it/300x300&text=Whoops!+No+Image" 
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, default_url: "http://placehold.it/700x700&text=Whoops!+No+Image" 
   # has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, default_url: "http://placehold.it/300x300&text=Whoops!+No+Image"
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
