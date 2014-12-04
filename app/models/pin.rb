696e28dce70972013bb2a1a4d2ae078beb2d3427
Andrew Borstein <info@brownborgstudios.com>
2014-12-03 23:17:21 -0500

    Form Validators

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,6 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, default_url: "http://placehold.it/700x700&text=Whoops!+No+Image" 
   # has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, default_url: "http://placehold.it/300x300&text=Whoops!+No+Image"
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	validates :image, presence: true
+  	validates :description, presence: true
 end
